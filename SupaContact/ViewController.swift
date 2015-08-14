abd28a0b615bb57bc557b61ef2dfbc13e43625b3
Adrian <lp4ever@hotmail.com>
2015-08-14 14:22:28 -0300

    Initial Commit

---
+++ SupaContact/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SupaContact
+//
+//  Created by Adrian Lodge on 8/14/15.
+//  Copyright (c) 2015 Supatone Innovation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
